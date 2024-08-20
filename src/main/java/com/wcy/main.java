1b55bb154b2082e616489a990b410add923ed1c8
Weichunyan <weicy@digiwin.com>
2024-08-20 15:27:51 +0800

    学习git

---
+++ src/main/java/com/wcy/main.java
@@ -0,0 +1,34 @@
+package com.wcy;
+
+class Outer {
+
+    public int num = 1;
+
+    class Inner {
+
+        public int num = 2;
+
+        public void show() {
+
+            int num = 3;
+
+            System.out.println(num);
+
+            System.out.println(this.num);
+
+            System.out.println(Outer.this.num);
+
+        }
+
+    }
+
+}
+
+class InnerTest {
+
+    public static void main(String[] args) {
+
+        new Outer().new Inner().show();
+
+    }
+}
